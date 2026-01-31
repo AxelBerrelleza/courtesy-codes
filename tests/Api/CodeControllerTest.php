7bd0b2b502a8c566aab227e872b6e3ba7f0c50ba
AxelBerrelleza <xrodriguezberrelleza@gmail.com>
2026-01-30 20:36:38 -0600

    new Api/CodeControllerTest, verifies POST endpoint

---
+++ tests/Api/CodeControllerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Tests\Api;
+
+use App\Dto\CodeDto;
+use App\Factory\EventFactory;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Zenstruck\Foundry\Test\Factories;
+
+/** @todo handle other scenarios */
+class CodeControllerTest extends WebTestCase
+{
+    use Factories;
+
+    public function testHappyPath(): void
+    {
+        $client = static::createClient();
+        $endpoint = '/events/%d/courtesy-codes';
+        $event = EventFactory::randomOrCreate();
+
+        $codeDto = new CodeDto();
+        $codeDto->quantity = 10;
+        $codeDto->type = 'VIP';
+        $codeDto->zoneId = 'Main Stage';
+        $codeDto->expiresAt = new \DateTimeImmutable('+1 day');
+        $normalizer = $this->getContainer()->get(NormalizerInterface::class);
+        $codeArr = $normalizer->normalize($codeDto, format: 'array');
+        $client->request(
+            'POST',
+            \sprintf($endpoint, $event->getId()),
+            server: ['CONTENT_TYPE' => 'application/json'],
+            content: \json_encode($codeArr),
+        );
+        $response = json_decode($client->getResponse()->getContent(), true);
+        // dump($response);
+        $this->assertResponseIsSuccessful();
+        $keysToVerify = [
+            'id',
+            'uuid',
+            'quantity',
+            'type',
+            'zoneId',
+            'expiresAt',
+            'createdAt',
+            'event',
+        ];
+        foreach ($keysToVerify as $key)
+            $this->assertArrayHasKey($key, $response);
+    }
+}
