84e54cd9b757267bb40f98d5f62d81b05f4c9168
AxelBerrelleza <xrodriguezberrelleza@gmail.com>
2026-02-03 17:50:23 -0600

    new CourtesyCodeRedeemer service + service test

---
+++ src/Service/Code/CourtesyCodeRedeemer.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Service\Code;
+
+use App\Dto\PostRedeemDto;
+use App\Entity\Code;
+use App\Entity\RedeemedCode;
+use App\Entity\User;
+use App\Enum\CodeStatus;
+use App\Enum\GuestType;
+
+class CourtesyCodeRedeemer
+{
+    public function redeemAvailableCode(
+        Code $code,
+        PostRedeemDto $redeemDto,
+        User $redeemedBy,
+        ?User $userOwner = null,
+    ) {
+        /** @todo verify code expiration */
+        $code->setStatus(CodeStatus::ALREADY_REDEEMED);
+        $redeemedCode = new RedeemedCode();
+        $redeemedCode->setCode($code);
+        $redeemedCode->setRedeemedAt(new \DateTimeImmutable());
+        $redeemedCode->setRedeemedBy($redeemedBy);
+        $redeemedCode->setUserOwner($userOwner);
+        $redeemedCode->setGuestName($redeemDto->guestName);
+        $redeemedCode->setGuestEmail($redeemDto->guestEmail);
+        $redeemedCode->setGuestType(GuestType::tryFrom($redeemDto->guestType));
+
+        $code->setRedeemedCode($redeemedCode);
+    }
+}
---
+++ tests/CourtesyCodeRedeemerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Tests;
+
+use App\Dto\PostRedeemDto;
+use App\Enum\GuestType;
+use App\Factory\CodeFactory;
+use App\Factory\UserFactory;
+use App\Service\Code\CourtesyCodeRedeemer;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Zenstruck\Foundry\Test\Factories;
+
+use function Zenstruck\Foundry\faker;
+
+class CourtesyCodeRedeemerTest extends KernelTestCase
+{
+    use Factories;
+
+    public function testRedeemAvailableCode(): void
+    {
+        $code = CodeFactory::createOne();
+        $this->assertNull($code->getRedeemedCode());
+        $redeemDto = new PostRedeemDto();
+        $redeemDto->guestName = faker()->name();
+        $redeemDto->guestEmail = faker()->email();
+        $redeemDto->guestType = faker()->shuffleArray(GuestType::values())[0];
+        $courtesyCodeRedeemer = $this->getContainer()->get(CourtesyCodeRedeemer::class);
+        $courtesyCodeRedeemer->redeemAvailableCode($code, $redeemDto, UserFactory::random());
+        $this->assertNotNull($code->getRedeemedCode());
+    }
+}
