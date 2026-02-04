de5fecbdb20483ca335541858270f819cc49f34b
AxelBerrelleza <xrodriguezberrelleza@gmail.com>
2026-02-03 20:42:53 -0600

    removed unnecessary todos from EventFactory

--- src/Factory/EventFactory.php
+++ src/Factory/EventFactory.php
@@ -14,8 +14,6 @@ final class EventFactory extends PersistentObjectFactory
 {
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
-     *
-     * @todo inject services if required
      */
     public function __construct() {}
 
@@ -27,14 +25,12 @@ final class EventFactory extends PersistentObjectFactory
 
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
-     *
-     * @todo add your default values here
      */
     #[\Override]
     protected function defaults(): array|callable
     {
         return [
-            'name' => self::faker()->text(255),
+            'name' => self::faker()->country(),
             'date' => \DateTimeImmutable::createFromMutable(self::faker()->dateTimeBetween('now', '+7 days')),
             'status' => EventStatus::ACTIVE,
         ];
